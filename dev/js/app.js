89359bec332520f6a21f4486a1c54766b8815222
Richard Stephenson <richard.stephenson@akqa.com>
2014-11-01 22:30:45 -0700

    No jquery. Off canvas nav. Benton

--- dev/js/app.js
+++ dev/js/app.js
@@ -1,11 +1,34 @@
-var nike = nike || {};
-nike.nwm = nike.nwm || {};
+var rps = rps || {};
 
-nike.nwm.base = (function() {
+rps.lib = (function (document) {
     'use strict';
 
-    (function() {
-        console.log('The app JS is running successfully');
-    }());
+    var toggleClass = function (element, className) {
+        element.classList.toggle(className);
+    },
 
-}());
+    createEvent = function (eventName) {
+        var ev;
+        if (typeof CustomEvent === 'function') {
+            ev = new CustomEvent(eventName);
+        } else {
+            ev = document.createEvent('HTMLEvents');
+            ev.initEvent(eventName, true, true);
+        }
+        return ev;
+    },
+
+    addEventListeners = function (selector, eventType, functionName) {
+        var elements = document.querySelectorAll(selector);
+        for (var i = 0; i < elements.length; i++) {
+            elements[i].addEventListener(eventType, functionName);
+        }
+    };
+
+    return {
+        toggleClass: toggleClass,
+        addEventListeners: addEventListeners,
+        createEvent: createEvent
+    };
+
+})(window.document);
