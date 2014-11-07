0a7d937df5f6826bd02cbd44e74efb97b5bb39eb
Richard Stephenson <Richard.Stephenson@akqa.com>
2014-11-07 14:01:06 -0800

    Removed unused Nav functions. Tidied up

--- dev/js/modules/module.nav.js
+++ dev/js/modules/module.nav.js
@@ -3,55 +3,23 @@ var rps = rps || {};
 rps.nav = (function (window, $) {
     'use strict';
 
-    var $window = $(window),
-        $body = $('body'),
-        $nav = $('#menu'),
+    var $html = $('html'),
 
-    hijackLinks = function () {
-        $('html').on('click', 'a[href^="/"]', checkInternalLink);
+    hijackInternalLinks = function () {
+        $html.on('click', 'a[href^="/"]', checkInternalLink);
     },
 
-    // Check to see if we are not already on this page before loading a new one
+    // Check to see link is to another page
     checkInternalLink = function (e) {
-        var newPath = this.pathname,
-            currentPath = window.location.pathname;
         e.preventDefault();
         e.stopPropagation();
-        if (newPath !== currentPath) {
-            rps.page.go(newPath);
+        if (this.pathname !== window.location.pathname) {
+            rps.page.go(this.pathname);
         }
     },
 
-    openCloseNav = function () {
-        $('.no-touch body').on('click', '#showNav', function (e) {
-            e.preventDefault();
-            $body.toggleClass('show-menu');
-        });
-        $('.touch body').on('touchstart', '#showNav', function (e) {
-            e.preventDefault();
-            $body.toggleClass('show-menu');
-        });
-    },
-
-    moveMenu = function () {
-        var menu = $('#menu').remove();
-        $('header').after(menu);
-    },
-
-    showHideNav = function () {
-        $window.on('add-nav', function () {
-            $body.addClass('show-menu');
-        });
-        $window.on('remove-nav', function () {
-            $body.removeClass('show-menu');
-        });
-    },
-
     init = (function () {
-        moveMenu();
-        showHideNav();
-        openCloseNav();
-        hijackLinks();
+        hijackInternalLinks();
     })();
 
 })(window, window.jQuery);
